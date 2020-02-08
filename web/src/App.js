9b81586b10aa2a7cba884c4c31523994731271cc
filippecg <filippecg88@gmail.com>
2020-02-08 12:25:41 -0300

    Criado layout para interface web

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,107 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div id="app">
+            <aside>
+                <strong>Cadastrar</strong>
+                <form>
+                    <div className="input-block">
+                        <label htmlFor="github_username">Usuário do Github</label>
+                        <input name="github_username" id="github_username" required />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="techs">Tecnologias</label>
+                        <input name="techs" id="techs" required />
+                    </div>
+
+                    <div className="input-group">
+                        <div className="input-block">
+                            <label htmlFor="Latitude">Latitude</label>
+                            <input name="Latitude" id="Latitude" required />
+                        </div>
+
+                        <div className="input-block">
+                            <label htmlFor="longitude">Longitude</label>
+                            <input name="longitude" id="longitude" required />
+                        </div>
+                    </div>
+
+                    <button type="submit">Salvar</button>
+                </form>
+            </aside>
+
+            <main>
+                <ul>
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars2.githubusercontent.com/u/59796180?s=460&v=4" alt="Filippe de Carvalho" />
+                            <div className="user-info">
+                                <strong>Filippe de Carvalho</strong>
+                                <span>PHP, HTML5, CSS</span>
+                            </div>
+                        </header>
+                        <p>Desenvolvedor de softwares | PHP full stack | Aprendendo a stack NodeJS, ReactJS, ReactNative</p>
+                        <a href="https://github.com/filippecg">Acessar perfil no Github</a>
+                    </li>
+
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars2.githubusercontent.com/u/59796180?s=460&v=4" alt="Filippe de Carvalho" />
+                            <div className="user-info">
+                                <strong>Filippe de Carvalho</strong>
+                                <span>PHP, HTML5, CSS</span>
+                            </div>
+                        </header>
+                        <p>Desenvolvedor de softwares | PHP full stack | Aprendendo a stack NodeJS, ReactJS, ReactNative</p>
+                        <a href="https://github.com/filippecg">Acessar perfil no Github</a>
+                    </li>
+
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars2.githubusercontent.com/u/59796180?s=460&v=4" alt="Filippe de Carvalho" />
+                            <div className="user-info">
+                                <strong>Filippe de Carvalho</strong>
+                                <span>PHP, HTML5, CSS</span>
+                            </div>
+                        </header>
+                        <p>Desenvolvedor de softwares | PHP full stack | Aprendendo a stack NodeJS, ReactJS, ReactNative</p>
+                        <a href="https://github.com/filippecg">Acessar perfil no Github</a>
+                    </li>
+
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars2.githubusercontent.com/u/59796180?s=460&v=4" alt="Filippe de Carvalho" />
+                            <div className="user-info">
+                                <strong>Filippe de Carvalho</strong>
+                                <span>PHP, HTML5, CSS</span>
+                            </div>
+                        </header>
+                        <p>Desenvolvedor de softwares | PHP full stack | Aprendendo a stack NodeJS, ReactJS, ReactNative</p>
+                        <a href="https://github.com/filippecg">Acessar perfil no Github</a>
+                    </li>
+
+                    <li className="dev-item">
+                        <header>
+                            <img src="https://avatars2.githubusercontent.com/u/59796180?s=460&v=4" alt="Filippe de Carvalho" />
+                            <div className="user-info">
+                                <strong>Filippe de Carvalho</strong>
+                                <span>PHP, HTML5, CSS</span>
+                            </div>
+                        </header>
+                        <p>Desenvolvedor de softwares | PHP full stack | Aprendendo a stack NodeJS, ReactJS, ReactNative</p>
+                        <a href="https://github.com/filippecg">Acessar perfil no Github</a>
+                    </li>
+                </ul>
+            </main>
+        </div>
+    );
 }
 
 export default App;
